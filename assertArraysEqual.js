ce6d855e81b90876222d7ba4417ddb24fd59a495
ahmaddaadaa <ahmadd@uvic.ca>
2023-05-30 21:01:20 -0700

    takes two arrays and checks equalaty

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual !== expected) {
+    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: [${actual}] !== [${expected}]`);
+    return;
+  }
+
+  console.log(`\u{2705}\u{2705}\u{2705} Assertion Passed: [${actual}] === [${expected}]`);
+  return;
+};
+
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((element, index) => element === arr2[index]);
+};
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+const assertArraysEqual = function(array1, array2) {
+
+  assertEqual(eqArrays(array1, array2), true);
+
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // calling the function
