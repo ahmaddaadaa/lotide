6aa59b8fef93c903acbd02ffd9b8a83c75f6e4f5
ahmaddaadaa <ahmadd@uvic.ca>
2023-06-26 14:07:05 -0700

    updated

--- assertEqual.js
+++ assertEqual.js
@@ -2,11 +2,11 @@
 const assertEqual = function(actual, expected) {
 
   if (actual !== expected) {
-    console.log("\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: [" + actual + "] !== [" + expected + "]");
+    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: [${actual}] !== [${expected}]`);
     return;
   }
 
-  console.log("\u{2705}\u{2705}\u{2705} Assertion Passed: [" + actual + "] === [" + expected + "]");
+  console.log(`\u{2705}\u{2705}\u{2705} Assertion Passed: [${actual}] === [${expected}]`);
   return;
 };
 
