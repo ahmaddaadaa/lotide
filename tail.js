a51e22d344e5ef6968040ae156ada991319409d9
ahmaddaadaa <ahmadd@uvic.ca>
2023-06-26 14:36:08 -0700

    updated

--- tail.js
+++ tail.js
@@ -1,26 +1,8 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-    return;
-  }
-
-  console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  return;
-};
-
 const tail = function(array) {
   let tailArr = array;
   tailArr.shift();
   return tailArr;
 };
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 
 module.exports = tail;
