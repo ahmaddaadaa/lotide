d683cd6f7cbab87201a341f0097a48b6e56be27b
ahmaddaadaa <ahmadd@uvic.ca>
2023-06-28 16:49:44 -0700

    updated

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,4 @@
-
+const assertEqual = require(`./assertEqual`);
 
 
 const bestTVShowsByGenre = {
@@ -8,53 +8,16 @@ const bestTVShowsByGenre = {
 };
 
 
-
-
-
-
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if (actual !== expected) {
-    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: [${actual}] !== [${expected}]`);
-    return;
-  }
-
-  console.log(`\u{2705}\u{2705}\u{2705} Assertion Passed: [${actual}] === [${expected}]`);
-  return;
-};
-
-
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  return arr1.every((element, index) => element === arr2[index]);
-};
-
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-const assertArraysEqual = function(array1, array2) {
-
-  assertEqual(eqArrays(array1, array2), true);
-
-};
-
-
-
 const findKeyByValue = function(obj, value) {
   for (let key in obj) {
-    
+
     if (obj[key] === value) {
-      
+
       return key;
     }
   }
 
-  
+
   return undefined;
 };
 
--- letterPositions.js
+++ letterPositions.js
@@ -1,19 +1,22 @@
+const assertArrysEqual = require(`./assertArraysEqual`);
 const letterPositions = function(sentence) {
   const results = {};
 
   for (let i = 0; i < sentence.length; i++) {
     const char = sentence[i].toLowerCase();
-    if (char.match(/[a-z]/i)) {
+    
       if (results[char]) {
         results[char].push(i);
       } else {
         results[char] = [i];
       }
-    }
+    
   }
   return results;
 };
 
+const results = letterPositions("Hello World");
 
-
-console.log(letterPositions("Hello World"));
+console.log(results);
+assertArrysEqual(results['h'],[0]);
+assertArrysEqual(results['l'],[2,3,9]);
